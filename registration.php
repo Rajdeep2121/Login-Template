c2ab1ee3448da2dea02f3f1806975549840bf5b5
Rajdeep Sengupta <senguptarajdeep21@gmail.com>
2020-06-16 21:11:33 +0530

    PDO for db

--- registration.php
+++ registration.php
@@ -1,20 +1,39 @@
 <?php
 session_start();
 // header('location:login.php');
-$con = mysqli_connect('postgres://bujdqnykpoacpa:69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833@ec2-34-197-141-7.compute-1.amazonaws.com:5432/db8iem6qdutl20
-','bujdqnykpoacpa','69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833
-','db8iem6qdutl20','69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833');
+// $con = mysqli_connect('postgres://bujdqnykpoacpa:69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833@ec2-34-197-141-7.compute-1.amazonaws.com:5432/db8iem6qdutl20
+// ','bujdqnykpoacpa','69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833
+// ','db8iem6qdutl20','69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833');
 
-// create db if it doesnt exist
-$createdb = "create database userregistration";
-if($con->query($createdb) === TRUE){
-    echo "";
-}
-else{
-    echo "";
-}
+// // create db if it doesnt exist
+// $createdb = "create database userregistration";
+// if($con->query($createdb) === TRUE){
+//     echo "";
+// }
+// else{
+//     echo "";
+// }
 
-mysqli_select_db($con,'userregistration');
+// mysqli_select_db($con,'userregistration');
+
+
+$dsn = "pgsql:"
+    . "host=ec2-34-197-141-7.compute-1.amazonaws.com;"
+    . "dbname=db8iem6qdutl20;"
+    . "user=bujdqnykpoacpa;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=69e98c3b8e683e0890cb73d939ecaafdfda1a10c21a5dc2de832b91287f7e833";
+
+$con = new PDO($dsn);
+
+
+
+
+
+
+
+
 
 // create table if it doesnt exist
 $create = "create table table2(name varchar(255),password varchar(255),age int,post varchar(255),salary int,email varchar(255), PRIMARY KEY(email))";
